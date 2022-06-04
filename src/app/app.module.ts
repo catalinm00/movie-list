0bac4d35df3a47b089fbd56b164454c897e9a8bd
catalinm00 <catalin.marian123@gmail.com>
2022-06-04 13:34:24 +0200

    creado model, service y components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListaComponent } from './components/lista/lista.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    DetailsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
